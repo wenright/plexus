ae48b58ce2883f19d445e83537f7633b5fb782ad
Will Enright <wenright@purdue.edu>
2016-03-29 21:32:43 -0400

    Update network code

--- network.lua
+++ network.lua
@@ -1,42 +1,103 @@
 local socket = require('socket')
 
-local network = {
+local Network = {
   callbacks = {},
-  id = 0
+  id = 0,
+  sendrate = 0.1,
+  localVariables = {},
+  variables = {}
 }
 
 local udp = socket.udp()
 assert(udp, 'Failed setting up UDP socket')
 udp:settimeout(0)
 
-function network.connect(ip, port, callback)
+function Network.connect(ip, port)
   udp:setpeername(ip, port)
 
-  network.send('join', { os.time() })
+  Network.time = 0
 
-  if callback then callback() end
+  Network.send('join', { os.time() })
+
+  Network.isConnected = false
+  Network.on('acknowledgeJoin', function(msg)
+    Network.isConnected = true
+    Network.id = msg
+  end)
+
+  repeat until Network.isConnected or Network.update() == -1
+
+  if Network.isConnected then
+    Timer.every(Network.sendrate, function()
+      Network.send('update', Network.localVariables)
+    end)
+
+    Network.log('Connected to server')
+
+    Network.instantiate('Player', {x = 0, y = 0})
+  else
+    Network.log('Unable to connect to server')
+    Gamestate.switch(Menu)
+  end
 end
 
-function network.on(cmd, callback)
-  network.callbacks[cmd] = callback
+function Network.on(cmd, callback)
+  Network.callbacks[cmd] = callback
 end
 
-network.on('assignID', function(params)
-  network.id = tonumber(params:match("%d*"))
+Network.on('update', function(params)
+  for objectID, newValue in pairs(params) do
+    local v = Network.variables[objectID]
+    if v and v._listen then
+      if v._interpolate then
+        Timer.tween(Network.sendrate, v[v._listen], newValue, 'linear')
+      else
+        v[v._listen] = newValue
+      end
+    end
+  end
 end)
 
+Network.on('instantiate', function(obj, playerID)
+  assert(Objects[obj.type], 'Object of type ' .. tostring(obj.type) .. ' does not exist.')
+  assert(obj.properties, 'Must assign properties when instantiating a new object')
 
-function network.send(cmd, params)
-  local msg = network.id .. ' ' .. cmd
+  obj.properties.isLocalPlayer = playerID == Network.id
+  obj.properties.id = obj.id
 
-  for i, param in pairs(params) do
-    msg = msg .. ' ' .. tostring(param)
-  end
+  Game.entities:add(obj.id, Objects[obj.type](obj.properties))
+end)
+
+Network.on('destroy', function(msg)
+  Game.entities:remove(msg.id)
+end)
+
+function Network.watch(objectID, t)
+  Network.localVariables[objectID] = t
+end
+
+function Network.listen(objectID, name, t, interpolate)
+  Network.variables[objectID] = t
+  Network.variables[objectID]._listen = name
+  Network.variables[objectID]._interpolate = interpolate
+end
+
+function Network.send(cmd, params)
+  local msg = Network.id .. ' ' .. cmd .. ' ' .. Serialize(params)
 
   udp:send(msg)
 end
 
-function network.update()
+function Network.instantiate(t, properties)
+  Network.send('instantiate', {type = t, properties = properties})
+end
+
+function Network.destroy(id)
+  Network.send('destroy', {id = id})
+end
+
+function Network.update(dt)
+  -- Receive data
   local data, err
 
   repeat
@@ -45,13 +106,28 @@ function network.update()
     if data then
       local id, cmd, params = data:match('^(%S*) (%S*) (.*)')
 
-      if network.callbacks[cmd] then
-        network.callbacks[cmd](params)
+      if Network.callbacks[cmd] then
+        Network.callbacks[cmd](Deserialize(params), tonumber(id))
+      else
+        Network.log('Unknown command "' .. cmd .. '"')
       end
     elseif err ~= 'timeout' then
-      error('Network error: ' .. tostring(err))
+      -- TODO show some kind of toast to tell user there was an error
+      print('Network error: ' .. tostring(err))
+      return -1
     end
   until not data
 end
 
-return network
+function Deserialize(ser)
+  return setfenv(loadstring(ser), {})()
+end
+
+function Network.log(str)
+  -- TODO write to file
+  if DEBUG then
+    print(str)
+  end
+end
+
+return Network
