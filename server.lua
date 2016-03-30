86e7fc138082c34f890357c865f0ec6485cb68b3
Will Enright <wenright@purdue.edu>
2016-03-29 21:31:47 -0400

    Update server code

--- server.lua
+++ server.lua
@@ -1,54 +1,107 @@
 local socket = require('socket')
 
-local server = {
+local Server = {
   callbacks = {},
-  id = -1
+  id = -1,
+  players = {},
+  numPlayers = 0,
+  udp = nil,
+  Serialize = Serialize
 }
 
-math.randomseed(os.time())
+local DEBUG = true
 
-local udp = socket.udp()
-assert(udp, 'UDP creation failed')
-udp:settimeout(0)
+function Server.start()
+  math.randomseed(os.time())
 
-udp:setsockname('*', 3000)
+  Server.udp = socket.udp()
+  assert(Server.udp, 'UDP creation failed')
+  Server.udp:settimeout(0)
 
-print('Server started')
+  Server.udp:setsockname('*', 3000)
 
-function server.on(cmd, callback)
-  server.callbacks[cmd] = callback
+  Server.log('Server started')
 end
 
-server.on('join', function(id, ip)
-  -- When a new user joins the room, assign them an ID
-  -- TODO make sure that this ID doesn't already exist
-  return 'assignID', { math.random(999999) }
-end)
+function Server.on(cmd, callback)
+  Server.callbacks[cmd] = callback
+end
 
-function server.send(ip, port, cmd, params)
-  local msg = server.id .. ' ' .. cmd
-  for i, param in pairs(params) do
-    msg = msg .. ' ' .. param
+function Server.send(ip, port, cmd, params, playerID)
+  local msg = playerID .. ' ' .. cmd .. ' ' .. params
+
+  Server.udp:sendto(msg, ip, port)
+end
+
+function Server.broadcast(cmd, params, playerID)
+  for i, player in pairs(Server.players) do
+    Server.send(player.ip, player.port, cmd, params, playerID)
   end
-
-  udp:sendto(msg, ip, port)
 end
 
-function server.update()
-  local data, ip, port = udp:receivefrom()
+function Server.instantiate(params, senderID)
+  local id
+  repeat
+    id = Server.newID()
+  until not Server.entities[id]
+
+  local t = Server.Deserialize(params)
+
+  t.id = id
+  t.ownerID = senderID
+  Server.entities[id] = t
+
+  params = Server.Serialize(t)
+
+  -- TODO send to other clients with a delay?
+  Server.broadcast('instantiate', params, senderID)
+end
+
+function Server.update()
+  local data, ip, port = Server.udp:receivefrom()
 
   if data then
-    local id, cmd, params = data:match('^(%S*) (%S*) (.*)')
+    local playerID, cmd, params = data:match('^(%S*) (%S*) (.*)')
 
-    if server.callbacks[cmd] then
-      local msg, params = server.callbacks[cmd](id, ip, params)
+    if Server.callbacks[cmd] then
+      local msg, params = Server.callbacks[cmd](params, playerID, ip, port)
       if msg then
-        server.send(ip, port, msg, params or {})
+        Server.send(ip, port, msg, params, playerID)
       end
+    else
+      Server.broadcast('update', params, playerID)
     end
-  elseif ip ~= 'timeout' then
+  elseif ip == 'closed' then
+    -- This should mean that the player has disconnected
+  elseif ip == 'timeout' then
+    -- This could mean that no one is connected
+  elseif ip == 'refused' then
+    -- This probably just means there are no clients connected
+  else
     error('Unknown network error: ' .. tostring(ip))
+  end
+
+  socket.sleep(0.01)
+end
+
+function Server.Deserialize(ser)
+  return setfenv(loadstring(ser), {})()
+end
+
+function Server.newID()
+  -- TODO better new id method
+  local id
+  repeat
+    id = math.random(999999)
+  until not Server.entities[id]
+  return id
+end
+
+function Server.log(str)
+  -- TODO write to log file
+  if DEBUG then
+    print(str)
   end
 end
 
-return server
+return Server
