69ac5e045518affb58078ddd468e4b5b1bef0d6f
Will Enright <wenright@purdue.edu>
2016-04-01 12:15:45 -0400

    Wall collisions

--- example/ball.lua
+++ example/ball.lua
@@ -1,11 +1,18 @@
 local Ball = {}
 
-function Ball:new()
+local speed = 1000
+local radius = 15
+
+function Ball:new(properties)
   local ball = {
-    x = love.graphics.getWidth() / 2,
-    y = love.graphics.getHeight() / 2,
+    position = {
+      x = love.graphics.getWidth() / 2,
+      y = love.graphics.getHeight() / 2
+    },
     vx = love.math.random(),
-    vy = love.math.random()
+    vy = love.math.random(),
+    id = properties.id,
+    isLocalPlayer = properties.isLocalPlayer
   }
 
   setmetatable(ball, self)
@@ -17,13 +24,18 @@ end
 function Ball:update(dt)
   if self.isLocalPlayer then
     -- Update position
-    self.x, self.y = self.x + self.vx * dt * speed, self.y + self.vy * dt * speed
+    self.position.x, self.position.y = self.position.x + self.vx * dt * speed, self.position.y + self.vy * dt * speed
+
+    -- Perform collision detection with top and bottom walls
+    if self.position.y > love.graphics.getHeight() - radius or self.position.y < radius then
+      self.vy = self.vy * -1
+    end
   end
 end
 
 function Ball:draw()
   love.graphics.setColor(255, 255, 255)
-  love.graphics.circle('fill', self.x, self.y, 15, 25)
+  love.graphics.circle('fill', self.position.x, self.position.y, radius)
 end
 
 return Ball
