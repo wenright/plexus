6b698d5fb23d4cbe90f441489af09579299d1ad4
Will Enright <wenright@purdue.edu>
2016-04-04 22:12:49 -0400

    Add some comments to example

--- example/client/main.lua
+++ example/client/main.lua
@@ -1,16 +1,18 @@
 local Network = require 'network'
 
 Network.on('connected', function()
-  -- Do things here
+  print('This function is called as soon as the client connects to the server')
 end)
 
 Network.connect('127.0.0.1', 3000)
 
 function love.update(dt)
+  -- Update reads in any messages received from the server
   Network.update()
 end
 
 function love.draw()
+  -- Network.isConnected is updated once the client receives an acknowledgement from the server
   if Network.isConnected then
     love.graphics.print('Connected to the server')
   else
@@ -19,5 +21,6 @@ function love.draw()
 end
 
 function love.quit()
+  -- You should call close on quit to let the server know that this client has left
   Network.close()
 end
